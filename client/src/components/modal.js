65a5bbddabf3dc18a1393085703441e17b0c93d1
kori-njoroge <korinjoroge63@gmail.com>
2022-12-03 15:38:01 +0300

    Notfications

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Modal.css";
+import logo from '../images/user.png'
+import Axios from "axios";
+import ApiLink from "./link";
 
 export default function Modal({ setOpenModal }) {
-    console.log(setOpenModal)
+    const currentUser = window.localStorage.getItem("username")
+    const current = new Date();
+    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
 
-    const styles = {
-        overlay:{
-            zIndex: 1000
-        }
-    }
+    useEffect( () =>{
+        Axios.get(`${ApiLink}/notifications`).then(response =>{
+            console.log(response);
+        }).catch(err =>{
+            console.log(err);
+        })
+    },[])
+
     
 return (
     <div className="modalBackground">
-    <div className="modalContainer" style={{styles}}>
+    <div className="modalContainer" >
         <div className="titleCloseBtn">
         <button
             onClick={() => {
@@ -23,11 +31,64 @@ return (
         </button>
         </div>
         <div className="title">
-        <h3>Are You Sure You Want to Continue?</h3>
+        <h3>Notifications</h3>
+        {/* <hr width="40%" size="1" color="white"/> */}
         </div>
-        <div className="bdy">
-        <p>The next page looks amazing. Hope you want to go there!</p>
-        </div>
+        <p className="fieldset--title">Welcome {currentUser}</p>
+        <p className="fieldset--date">{date}</p>
+        <div>
+        <fieldset className="bdy">
+
+            <div className="content--holder">
+            <i id="admin--icon" className="fas fa-user "></i>
+            <div>
+            <fieldset className="content--info">
+                Welcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome tomorrow we jumping aroundWelcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome tomorrow we jumping around
+            </fieldset>
+            <small className="small--details">{currentUser} {date} {date}</small>
+            </div>
+            </div>
+
+            <div className="content--holder">
+            <i id="admin--icon" className="fas fa-user "></i>
+            <div>
+            <fieldset className="content--info">
+                Welcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome tomorrow we jumping aroundWelcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome tomorrow we jumping around
+            </fieldset>
+            <small><b>{currentUser} {date} {date}</b></small>
+            </div>
+            </div>
+
+            <div className="content--holder">
+            <i id="admin--icon" className="fas fa-user "></i>
+            <div>
+            <fieldset className="content--info">
+                Welcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome tomorrow we jumping aroundWelcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome tomorrow we jumping around
+            </fieldset>
+            <small className="small--details">{currentUser} {date} {date}</small>
+            </div>
+            </div>
+            <div className="content--holder">
+            <i id="admin--icon" className="fas fa-user "></i>
+            <div>
+            <fieldset className="content--info">
+                Welcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome tomorrow we jumping aroundWelcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome tomorrow we jumping around
+            </fieldset>
+            <small className="small--details">{currentUser} {date} {date}</small>
+            </div>
+            </div>
+            <div className="content--holder">
+            <i id="admin--icon" className="fas fa-user "></i>
+            <div>
+            <fieldset className="content--info">
+                Welcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome tomorrow we jumping aroundWelcome tomorrow we jumping around Welcome tomorrow we jumping around Welcome 
+            </fieldset>
+            <small className="small--details">{currentUser} {date} {date}</small>
+            </div>
+            </div>
+
+        </fieldset>
+            </div>
         <div className="footer">
         <button
             onClick={() => {
